8baa2236135bc8c405ed61984d50dcf8eb31eeb5
RubioJr9 <tpat@sci.utah.edu>
2022-10-11 19:40:51 -0600

    Uncomment def

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -398,8 +398,8 @@ U_UND,             U_CUT,             U_CPY,             U_PST,             U_RD
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
 
-/* #define MIRYOKU_ALTERNATIVES_RIGHTY \ */
-/* &kp Q,             &kp U,             &kp COMMA,         &kp L,             &kp Z,             &kp K,             &kp F,             &kp Y,             &kp C,             &tog RIGHTY,       \ */
-/* &hm LGUI O,        &hm LALT I,        &hm LCTRL A,       &hm LSHFT N,       &kp W,             &kp P,             &hm LSHFT H,       &hm LCTRL T,       &hm LALT S,        &hm LGUI R,        \ */
-/* &lt U_BUTTON U_NU, &hm RALT DEL,      &kp DOT,           &kp M,             &kp X,             &kp B,             &kp D,             &kp G,             &hm RALT V,        &lt U_BUTTON J,    \ */
-/* U_NP,              U_NP,              &lt U_MEDIA BSPC,  &lt U_NAV SPC,     &lt U_MOUSE RET,   &lt U_SYM ESC,     &lt U_NUM E,       &lt U_FUN TAB,     U_NP,              U_NP */
+#define MIRYOKU_ALTERNATIVES_RIGHTY \
+&kp Q,             &kp U,             &kp COMMA,         &kp L,             &kp Z,             &kp K,             &kp F,             &kp Y,             &kp C,             &tog RIGHTY,       \
+&hm LGUI O,        &hm LALT I,        &hm LCTRL A,       &hm LSHFT N,       &kp W,             &kp P,             &hm LSHFT H,       &hm LCTRL T,       &hm LALT S,        &hm LGUI R,        \
+&lt U_BUTTON U_NU, &hm RALT DEL,      &kp DOT,           &kp M,             &kp X,             &kp B,             &kp D,             &kp G,             &hm RALT V,        &lt U_BUTTON J,    \
+U_NP,              U_NP,              &lt U_MEDIA BSPC,  &lt U_NAV SPC,     &lt U_MOUSE RET,   &lt U_SYM ESC,     &lt U_NUM E,       &lt U_FUN TAB,     U_NP,              U_NP
